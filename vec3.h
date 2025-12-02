5a58eb7cb96ed8aed6342537054ffdb09af661b3
Thales Pinto <thales.moreira1998.2@gmail.com>
2025-12-02 11:43:36 -0300

    função pra verificar se o vetor gerado esta no emisferio certo

--- vec3.h
+++ vec3.h
@@ -108,4 +108,12 @@ inline vec3 random_unit_vector() {
     }
 }
 
+inline vec3 random_on_hemisphere(const vec3& normal) {
+    vec3 on_unit_sphere = random_unit_vector();
+    if (dot(on_unit_sphere, normal) > 0.0) // In the same hemisphere as the normal
+        return on_unit_sphere;
+    else
+        return -on_unit_sphere;
+}
+
 #endif
